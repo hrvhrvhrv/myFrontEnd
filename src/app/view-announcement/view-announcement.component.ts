84b31175a50a31a91cce18f7ef5b0b73c311f036
Craig Harvey <craig.harvey.18@gmail.com>
2018-02-23 10:55:03 +0000

    Friday morning change of commentss

---
+++ src/app/view-announcement/view-announcement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-announcement',
+  templateUrl: './view-announcement.component.html',
+  styleUrls: ['./view-announcement.component.css']
+})
+export class ViewAnnouncementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
